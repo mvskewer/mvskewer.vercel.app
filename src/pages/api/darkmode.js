6cbcdc4f6a0e398138b633d2370a23f8c1cb4b4a
Rural Anemone <me.awesomerest@gmail.com>
2023-03-28 20:04:01 +0000

    uuuuuuuuuuuuuuuuu

--- src/pages/api/darkmode.js
+++ src/pages/api/darkmode.js
@@ -7,7 +7,10 @@ const openai = new OpenAIApi(configuration);
 
 const response = await openai.createCompletion({
 	model: "text-davinci-003",
-	prompt: "insult someone who tried to switch from dark mode to light mode, and then let them know, unapologetically, that they can't switch to light mode",
+	prompt: `\
+insult a user who tried to switch from dark mode to light mode. make it sarcastic.
+let them know that they can't switch to light mode.
+imply that the user should feel bad about choosing to switch from dark mode.`,
 	temperature: 0.7,
 	max_tokens: 128,
 	top_p: 1,
@@ -17,7 +20,7 @@ const response = await openai.createCompletion({
 
 // ---
 
-const output = response.data.choices[0].text.replace(/^\n+/,"");
+const output = response.data.choices[0].text.replace(/^\n+/, "").replaceAll("\"", ""); // for some reason it really likes quotes
 
 export default function handler(req, res) {
 	res.status(200).json({ output: output });
