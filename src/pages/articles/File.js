80a5d3932dbd9adfb5cf64e266a830f69272cb1a
Rural Anemone <me.awesomerest@gmail.com>
2023-03-29 22:25:10 +0000

    some more things I guess idk

---
+++ src/pages/articles/File.js
@@ -0,0 +1,9 @@
+// import styles from '@/styles/articles/File.module.css'
+
+const File = props => {
+    return (
+        <p id={props}>hi</p>
+    )
+}
+
+export default File;
---
+++ src/pages/articles/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import File from './File'
+// import styles from '@/styles/articles/File.module.css'
+
+// `npx serve .` somewhere???
+
+
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <meta />
+      </Head>
+      <p>hi</p>
+    </>
+  )
+}
