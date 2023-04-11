b0b9f1c546252ffaa6eca0310c525403f97e4e9e
RuralAnemone ‮ <me.awesomerest@gmail.com>
2023-04-11 14:22:27 +0000

    fix particles across pages

--- public/particles.js
+++ public/particles.js
@@ -1,15 +1,11 @@
-document.addEventListener("readystatechange", event => {
-    if (event.target.readyState === "complete") {
-        setParticles();
-    }
-});
-
-if (document.readyState === "complete") setInterval(setParticles, 250); // in case everything loads before this???
+setInterval(setParticles, 250);
 
 async function setParticles() {
-    const particlesDiv = document.querySelector("#particles-js");
-    if (particlesDiv !== null) {
-        particlesJS('particles-js', await fetch('/particlesjs-config.json').then(res => res.json()));
-        particlesDiv.id = "#particles-injected";
+if (document.readyState === "complete") {
+        const particlesDiv = document.querySelector("#particles-js");
+        if (particlesDiv !== null) {
+            particlesJS('particles-js', await fetch('/particlesjs-config.json').then(res => res.json()));
+            particlesDiv.id = "#particles-injected";
+        }
     }
 }
