48812641bb46647925e276bd5cab98741fd89944
RuralAnemone <me.awesomerest@gmail.com>
2023-04-26 08:37:21 -0500

    navbar, fix footer icons being jank

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,40 +1,45 @@
-import styles from '@/styles/Page.module.css'
-import Head from 'next/head'
-import Gtag from '@/components/Gtag'
-import Particles from '@/components/Particles'
-import Footer from '@/components/Footer';
+import styles from "@/styles/Page.module.css";
+import Head from "next/head";
+import Gtag from "@/components/Gtag";
+import Particles from "@/components/Particles";
+import Footer from "@/components/Footer";
+import SkewerNavigationBar from "@/components/SkewerNavigationBar";
 
 export default function Page(props: any) {
 	return (
 		<>
-		<Head>
-			<meta charSet="UTF-8" />
-			<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-			<meta name="description" content={props.description} />
-  
-			<link rel="icon" type="image/x-icon" href="/favicon.ico" />
-			<title>{props.title} | The Skewer</title>
-		</Head>
-		
-		<Gtag />
-		<Particles />
-  
-		<div className={styles.main}>
-			<header>
-				<h1 className={styles.title}>{props.h1}</h1>
-				<h2 className={styles.title}>{props.h2}</h2>
-				<hr />
-			</header>
-			<main>
-				<div className={styles.page}>
-					{props.children}
-				</div>
-			</main>
-			<footer>
-				<Footer />
-			</footer>
-		</div>
-	  </>
+			<Head>
+				<meta charSet="UTF-8" />
+				<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+				<meta name="description" content={props.description} />
+
+				<link rel="icon" type="image/x-icon" href="/favicon.ico" />
+				<title>{props.title} | The Skewer</title>
+			</Head>
+
+			<Gtag />
+			<Particles />
+
+			<div className={styles.main}>
+				<nav>
+					<SkewerNavigationBar />
+				</nav>
+
+				<header>
+					<h1 className={styles.title}>{props.h1}</h1>
+					<h2 className={styles.title}>{props.h2}</h2>
+					<hr />
+				</header>
+
+				<main>
+					<div className={styles.page}>{props.children}</div>
+				</main>
+
+				<footer>
+					<Footer />
+				</footer>
+			</div>
+		</>
 	);
 }
---
+++ src/components/SkewerNavigationBar.tsx
@@ -0,0 +1,8 @@
+import Link from 'next/link'
+import styles from '@/styles/SkewerNavigationBar.module.css'
+
+export default function SkewerNavigationBar() {
+	return (
+		<div></div>
+	);
+}
