4fe448f7fa722d5c383de6b78fc2e09a8fb64754
Isaiah W <64103005+RuralAnemone@users.noreply.github.com>
2023-04-26 22:05:42 -0500

    mastodon me rel
    Signed-off-by: Isaiah W <64103005+RuralAnemone@users.noreply.github.com>

--- src/components/FooterIcon.tsx
+++ src/components/FooterIcon.tsx
@@ -5,7 +5,7 @@ import styles from '@/styles/FooterIcon.module.css'
 export default function FooterIcon(props: any) {
 	return (
 		<div className={styles.footerIcon} title={props.title}>
-			<Link href={props.href} target="_blank">
+			<Link href={props.href} target="_blank" rel="me">
 				<Image src={`/icons/${props.imageSrc}`} alt={props.alt} width="32" height="32" />
 			</Link>
 		</div>
