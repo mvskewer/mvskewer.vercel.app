f840345442f41b89eb75ef42034cdd8db8a64382
Isaiah W <64103005+RuralAnemone@users.noreply.github.com>
2023-04-28 17:29:46 +0000

    rework

--- public/js/particles.js
+++ public/js/particles.js
@@ -1,11 +1,9 @@
-setInterval(setParticles, 250);
+setInterval(setParticles, 10);
+const particlesDiv = document.querySelector("#particles-js");
 
 async function setParticles() {
-	if (document.readyState === "complete") {
-		const particlesDiv = document.querySelector("#particles-js");
-		if (particlesDiv !== null) {
-			particlesJS("particles-js", await fetch("/particlesjs-config.json").then(res => res.json()));
-			particlesDiv.id = "#particles-injected";
-		}
+	if (document.readyState === "complete" && particlesDiv !== null) {
+		await particlesJS("particles-js", await fetch("/particlesjs-config.json").then(res => res.json()));
+		particlesDiv.id = "#particles-injected";
 	}
 }
