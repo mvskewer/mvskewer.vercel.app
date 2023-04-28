adbaf7d47853852d3501589c807f4953b7059a3e
Isaiah W <64103005+RuralAnemone@users.noreply.github.com>
2023-04-28 17:20:04 +0000

    fix indent

--- public/js/particles.js
+++ public/js/particles.js
@@ -1,11 +1,11 @@
 setInterval(setParticles, 250);
 
 async function setParticles() {
-if (document.readyState === "complete") {
-        const particlesDiv = document.querySelector("#particles-js");
-        if (particlesDiv !== null) {
-            particlesJS('particles-js', await fetch('/particlesjs-config.json').then(res => res.json()));
-            particlesDiv.id = "#particles-injected";
-        }
-    }
+	if (document.readyState === "complete") {
+		const particlesDiv = document.querySelector("#particles-js");
+		if (particlesDiv !== null) {
+			particlesJS("particles-js", await fetch("/particlesjs-config.json").then(res => res.json()));
+			particlesDiv.id = "#particles-injected";
+		}
+	}
 }
