9009d4d47067731cb29c73dd037d69e3de697988
Isaiah W <64103005+RuralAnemone@users.noreply.github.com>
2023-09-12 23:43:26 +0000

    create /apply

---
+++ src/pages/apply/index.tsx
@@ -0,0 +1,12 @@
+import Page from "@/components/Page";
+import Link from "next/link";
+
+export default function Home() {
+	return (
+		<Page description="Apply to join our team!" title="Apply" h1="Applications" h2="Apply to join our team!">
+			<ol>
+				<li>h</li>
+			</ol>
+		</Page>
+	);
+}
