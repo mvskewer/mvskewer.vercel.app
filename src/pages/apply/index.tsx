8df9aa03f1fd04ec8a195bae3868c8ef7063c84f
Isaiah W <64103005+RuralAnemone@users.noreply.github.com>
2023-09-13 00:00:46 +0000

    "finish" /apply

--- src/pages/apply/index.tsx
+++ src/pages/apply/index.tsx
@@ -4,8 +4,13 @@ import Link from "next/link";
 export default function Home() {
 	return (
 		<Page description="Apply to join our team!" title="Apply" h1="Applications" h2="Apply to join our team!">
-			<ol>
-				<li>h</li>
+			<p>Hey! Thanks for visiting our page and reading our paper. If you&apos;re interested in joining our team, you can follow the steps below:</p>
+			<br />
+			<ol start={0}>
+				<li>Fill out the <Link href="https://forms.gle/2A7cjpCUePy3hByu5">Google Form</Link></li>
+				<li>If we think you&apos;re the stuff, we&apos;ll invite you to a private Google Chat to conduct an interview
+					<li><h6>note that you can request to do the interview on call or in-person if you really want to</h6></li>
+				</li>
 			</ol>
 		</Page>
 	);
