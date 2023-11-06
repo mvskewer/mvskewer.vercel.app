8424f49d1f2cb3cc01f84f0ae65d0f8dccb341ab
Isaiah W <ruralanemone@gmail.com>
2023-11-06 17:12:27 -0600

    rename to FileLink

--- src/pages/issues/archive/index.tsx
+++ src/pages/issues/archive/index.tsx
@@ -1,4 +1,4 @@
-import File from '@/components/File'
+import FileLink from '@/components/FileLink'
 import Page from '@/components/Page'
 
 export default function Home() {
@@ -11,9 +11,9 @@ export default function Home() {
     >
         <p>sorted by month:</p>
         <ul>
-          <File path="2023/February.pdf" name="February">February</File>
-          <File path="2023/March.pdf" name="March">March</File>
-          <File path="2023/September.pdf" name="September">September</File>
+          <FileLink path="2023/February.pdf" name="February">February</FileLink>
+          <FileLink path="2023/March.pdf" name="March">March</FileLink>
+          <FileLink path="2023/September.pdf" name="September">September</FileLink>
         </ul>
     </Page>
   )
