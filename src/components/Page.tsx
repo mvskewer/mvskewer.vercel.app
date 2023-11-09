0bb964d0176c27e4d2e8662e20e05650d4009dd1
Isaiah W <ruralanemone@gmail.com>
2023-11-09 08:02:00 -0600

    update how headers work in Page component

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -7,7 +7,17 @@ import Footer from "@/components/Footer";
 import NavBar from "@/components/NavBar";
 import Darkmode from "@/components/Darkmode";
 
-export default function Page(props: any) {
+export default function Page(props: {
+	description: string;
+	title: string;
+	h1?: string;
+	h2?: string;
+	h3?: string;
+	h4?: string;
+	h5?: string;
+	h6?: string;
+	children: any;
+}) {
 	return (
 		<>
 			<Head>
@@ -31,8 +41,12 @@ export default function Page(props: any) {
 				</nav>
 
 				<header>
-					<h1 className={styles.title}>{props.h1}</h1>
-					<h2 className={styles.title}>{props.h2}</h2>
+					{ props.h1 ? <h1 className={styles.title}>{props.h1}</h1> : <></> }
+					{ props.h2 ? <h2 className={styles.title}>{props.h2}</h2> : <></> }
+					{ props.h3 ? <h3 className={styles.title}>{props.h3}</h3> : <></> }
+					{ props.h4 ? <h4 className={styles.title}>{props.h4}</h4> : <></> }
+					{ props.h5 ? <h5 className={styles.title}>{props.h5}</h5> : <></> }
+					{ props.h6 ? <h6 className={styles.title}>{props.h6}</h6> : <></> }
 					<hr />
 				</header>
 
