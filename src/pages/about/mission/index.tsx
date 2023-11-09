eebe99d8ba5de4a41e01b00cc38d6e97e7577ad5
Isaiah W <ruralanemone@gmail.com>
2023-11-09 14:55:24 -0600

    dummy page

--- src/pages/about/mission/index.tsx
+++ src/pages/about/mission/index.tsx
@@ -1,7 +1,12 @@
 import Page from "@/components/Page";
 
 export default function Home() {
-	<Page>
-		<p>our mission</p>
-	</Page>
+	return (
+		<Page
+			description="our mission"
+			title="Our Mission"
+		>
+			<p>our mission</p>
+		</Page>
+	);
 }
