993af2027fd0c110e274d35bb331f299f5e0e94e
Isaiah W <ruralanemone@gmail.com>
2023-11-09 14:55:37 -0600

    do a simple design for staff page

--- src/pages/about/staff/[member]/index.tsx
+++ src/pages/about/staff/[member]/index.tsx
@@ -2,6 +2,7 @@ import { useRouter } from "next/router";
 import { redirect } from "next/navigation";
 import members from "@/config/members";
 import minimizeName from "@/functions/util/minimizeName";
+import Link from "next/link"
 
 import Page from "@/components/Page";
 
@@ -11,8 +12,13 @@ export default function Member() {
 
 	if (member === undefined) {
 		return (
-			<Page>
-				<p>loading...</p>
+			<Page
+				description="member data loading..."
+				title="loading..."
+				h1="‮"
+				h2="‮"
+			>
+				<p>‮</p>
 			</Page>
 		);
 	}
@@ -22,10 +28,16 @@ export default function Member() {
 
 	if (selectedMember === undefined)
 		return (
-			<Page>
+			<Page
+				description="This could be you!"
+				title="nobody"
+			>
 				<p>404</p>
 				<p>
 					member <code>{memberSlugString}</code> not found.
+				</p>
+				<p>
+					this could be you though! you should apply to <Link href="/apply">Join Our Team</Link>!
 				</p>
 			</Page>
 		);
@@ -34,8 +46,13 @@ export default function Member() {
 	// if (memberSlugString !== minimizeName(memberSlugString)) redirect(`/about/staff/${minimizeName(memberSlugString)}`);
 
 	return (
-		<Page>
-			<h1>{selectedMember?.name}</h1>
+		<Page
+			description={`${selectedMember.name}'s page`}
+			title={selectedMember.name}
+			h1={selectedMember.name}
+			h2={selectedMember.shortBio}
+		>
+			<p>{selectedMember.longBio}</p>
 		</Page>
 	);
 }
