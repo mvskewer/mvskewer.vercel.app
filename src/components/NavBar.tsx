2e6009c7b5cb795787f7f74b03b2c529fcdff2ba
Isaiah W <ruralanemone@gmail.com>
2023-11-09 15:36:26 -0600

    use router

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,18 +1,24 @@
 import Image from 'next/image'
 import styles from '@/styles/NavBar.module.css'
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 import dotdot from "@/functions/util/dotdot";
 
 export default function NavBar() {
+	const router = useRouter();
+	const currentUrl = router.asPath;
+
 	function handleClick() {
-		if (location.pathname === '/') alert('no?');
+		if (currentUrl === '/') alert('no?');
 		// setTimeout(history.back, 1000);
 	}
 	
+	console.log(currentUrl);
+
 	return (
 		<div onClick={handleClick}>
-			<Link href={dotdot(location.pathname)}>
+			<Link href={dotdot(currentUrl)}>
 				<Image
 					src="/icons/backbutton.svg"
 					className={styles.navbar}
