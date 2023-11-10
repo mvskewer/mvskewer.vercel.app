5101668a2898d8d94bfd8c3536e7cddc4536130d
Isaiah W <ruralanemone@gmail.com>
2023-11-10 15:41:02 -0600

    do a little design, realize I need flex

--- src/components/StaffCard.tsx
+++ src/components/StaffCard.tsx
@@ -1,18 +1,15 @@
-import styles from '@/styles/staff/StaffCard.module.css'
+import styles from "@/styles/staff/StaffCard.module.css";
 // import homeStyles from '@/styles/staff/Home.module.css'
-import Image from 'next/image'
+import Image from "next/image";
 
-export default function Bio(props: {
-  img: string;
-  name: string;
-  children: any;
-}) {
-  return (
-    <>
-      <Image src={`/staff/img/${props.img}.webp`} width="69" height="69" alt={`a picture of or for ${props.name}`} className={styles.img}></Image>
-      <p><b>{props.name}</b></p>
-      {props.children}
-      <br />
-    </>
-  );
+export default function Bio(props: { img: string; name: string; children: any }) {
+	return (
+		<div className={styles.cardContainer}>
+			<Image src={`/staff/img/${props.img}.webp`} width="69" height="69" alt={`a picture of or for ${props.name}`} className={styles.img}></Image>
+			<p>
+				<b>{props.name}</b>
+			</p>
+			{props.children}
+		</div>
+	);
 }
