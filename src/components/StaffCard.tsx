4846dd7bcaaa41c2faf998d28c5f7869a0f575bc
Isaiah W <ruralanemone@gmail.com>
2023-11-12 15:05:09 -0600

    rename to StaffCard finally I think maybe

--- src/components/StaffCard.tsx
+++ src/components/StaffCard.tsx
@@ -2,7 +2,7 @@ import styles from "@/styles/staff/StaffCard.module.css";
 // import homeStyles from '@/styles/staff/Home.module.css'
 import Image from "next/image";
 
-export default function Bio(props: { img: string; name: string; children: any }) {
+export default function StaffCard(props: { img: string; name: string; children: JSX.Element }) {
 	return (
 		<div className={styles.card}>
 			<Image src={`/staff/img/${props.img}.webp`} width="69" height="69" alt={`a picture of or for ${props.name}`} className={styles.img}></Image>
