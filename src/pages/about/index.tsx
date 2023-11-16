219f849522bc3d2a6e72e42907931dff1323dbcc
Diskid204 <125480682+Diskid204@users.noreply.github.com>
2023-11-16 11:14:56 -0600

    Update index.tsx - Capitalize link names
    Signed-off-by: Diskid204 <125480682+Diskid204@users.noreply.github.com>

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -5,10 +5,10 @@ export default function Home() {
 	return (
 		<Page description="about page asdfhalskdhdsfh" title="About" h1="About" h2="About the Skewer">
 			<p>
-				<Link href="/about/staff">staff</Link>
+				<Link href="/about/staff">Staff</Link>
 			</p>
 			<p>
-				<Link href="/about/mission">mission</Link>
+				<Link href="/about/mission">Our Mission</Link>
 			</p>
 		</Page>
 	);
