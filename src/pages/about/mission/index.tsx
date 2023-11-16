9c6b6cce8bff767fa2a6a29984c0862c5a691801
Isaiah W <ruralanemone@gmail.com>
2023-11-16 09:19:28 -0600

    update our mission (:

--- src/pages/about/mission/index.tsx
+++ src/pages/about/mission/index.tsx
@@ -3,7 +3,7 @@ import Page from '@/components/Page';
 export default function Home() {
 	return (
 		<Page description="our mission" title="Our Mission" h1="Our Mission" h2="Our Mission at The Skewer is um uh">
-			<p>- our mission</p>
+			<p>to be continued</p>
 		</Page>
 	);
 }
