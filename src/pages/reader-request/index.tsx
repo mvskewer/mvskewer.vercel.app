65fc25bf69d6b2a15ef308f73b7b77c9577fd1c5
Isaiah W <64103005+RuralAnemone@users.noreply.github.com>
2023-12-18 12:05:09 -0600

    placeholder garbage

---
+++ src/pages/reader-request/index.tsx
@@ -0,0 +1,14 @@
+import Page from "@/components/Page";
+import Link from "next/link"
+
+export default function ReaderRequest() {
+	return (
+		<Page description="request an article" title="Reader Request" h1="Reader Request Form" h2="Request an article here!">
+			<p>placeholder #2</p>
+			<p>spencer can do this idk lol</p>
+			<Link href="whatever the form is idk">link text</Link>
+			<p>hfasldkklfhjas</p>
+			<p>placeholder</p>
+		</Page>
+	)
+}
