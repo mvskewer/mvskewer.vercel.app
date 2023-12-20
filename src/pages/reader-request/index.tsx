4c4e19d025d7d2d561a245d63c6c5b378cfd3eb1
Diskid204 <125480682+Diskid204@users.noreply.github.com>
2023-12-19 18:32:16 -0600

    Update index.tsx - Form Link and Decriptions
    Signed-off-by: Diskid204 <125480682+Diskid204@users.noreply.github.com>

--- src/pages/reader-request/index.tsx
+++ src/pages/reader-request/index.tsx
@@ -4,9 +4,9 @@ import Link from 'next/link';
 export default function ReaderRequest() {
 	return (
 		<Page description="request an article" title="Reader Request" h1="Reader Request Form" h2="Request an article here!">
-			<p>placeholder #2</p>
-			<p>spencer can do this idk lol</p>
-			<Link href="whatever the form is idk">link text</Link>
+			<p>Submit an Article Idea For The Skewer!</p>
+			<p>Fill out the form below, and if we like it, we'll write about it!</p>
+			<Link href="whatever the form is idk">https://forms.gle/tNNtXoKQWb9fXpcBA</Link>
 			<p>hfasldkklfhjas</p>
 			<p>placeholder</p>
 		</Page>
