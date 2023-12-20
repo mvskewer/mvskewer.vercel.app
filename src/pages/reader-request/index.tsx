323e7795bedda83db8e33fc0a666997e61c7cfbc
Isaiah W <64103005+RuralAnemone@users.noreply.github.com>
2023-12-19 18:42:52 -0600

    no unescaped entities hehe titties

--- src/pages/reader-request/index.tsx
+++ src/pages/reader-request/index.tsx
@@ -5,7 +5,7 @@ export default function ReaderRequest() {
 	return (
 		<Page description="request an article" title="Reader Request" h1="Reader Request Form" h2="Request an article here!">
 			<p>Submit an Article Idea For The Skewer!</p>
-			<p>Fill out the form below, and if we like it, we'll write about it!</p>
+			<p>Fill out the form below, and if we like it, we&apos;ll write about it!</p>
 			<Link href="https://forms.gle/tNNtXoKQWb9fXpcBA">https://forms.gle/tNNtXoKQWb9fXpcBA</Link>
 		</Page>
 	);
