c7ee2f59f12130e9ce0654890b9efb7dd217e2a0
Isaiah <64103005+RuralAnemone@users.noreply.github.com>
2024-03-05 12:50:26 -0600

    add mission statement header

--- src/pages/about/mission/index.tsx
+++ src/pages/about/mission/index.tsx
@@ -2,8 +2,8 @@ import Page from '@/components/Page';
 
 export default function Home() {
 	return (
-		<Page description="our mission" title="Our Mission" h1="Our Mission" h2="Our Mission at The Skewer is um uh">
-			<p>to be continued</p>
+		<Page description="our mission" title="Our Mission" h1="Our Mission Statement" h2="We within the Skewer hold these values to the highest degree." h3="If an article or a person within our newspaper does not meet these values, they will not be tolerated and be immediately withdrawn from our organization." h4="If the Skewer ever violates one of these values, it will rescind from publishing as it has strayed from the vision of the founder.">
+				<h3>If the Skewer ever violates one of these values, it will rescind from publishing as it has strayed from the vision of the founder.</h3>
 		</Page>
 	);
 }
